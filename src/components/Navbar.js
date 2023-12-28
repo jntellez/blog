f81b81fa489c75b1face08d957c3c48dc51f85dd
Juan Tellez <juantellez916@gmail.com>
2023-12-28 00:37:54 -0600

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,97 +1,71 @@
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
-import { useAppContext } from '../store/store'
+import styled from "styled-components"
+import { NavLink } from "react-router-dom"
+import { useAppContext } from "../store/store"
 
 const Li = styled.li`
-    display: block;
-    color: #333;
-    list-style: none;
-    text-decoration: none;
-    line-height: 60px;
-    font-size: 20px;
-    transition: all 300ms ease;
-
-    &:hover {
-        background-color: #8083ff;
-        color: #fff;
-    }
+  display: block;
+  color: #333;
+  list-style: none;
+  text-decoration: none;
+  line-height: 60px;
+  font-size: 20px;
+  transition: all 300ms ease;
 `
 
 const Ul = styled.ul`
-    display: flex;
-    margin: 0 auto;
-    padding: 0;
+  display: flex;
+  margin: 0 auto;
+  padding: 0;
 `
 
-const styleLink = {
-    width: 'inherit',
-    height: 'inherit',
-    textDecoration: 'none',
-    color: 'inherit',
-    cursor: 'pointer',
-    padding: '20px 20px',
-}
+const StyledNavLink = styled(NavLink)`
+  display: inline-block;
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
+  box-sizing: border-box;
+  padding: 0 20px;
+  height: 100%;
 
-const activeLink = {
-    textDecoration: 'none',
-    color: '#fff',
-    backgroundColor: '#8083ff',
-    cursor: 'pointer',
-    padding: '16.5px 20px 16.3px 20px',
-}
+  &:hover {
+    background-color: #8083ff;
+    color: #fff;
+  }
+
+  &.active {
+    background-color: #8083ff;
+    color: #fff;
+  }
+`
 
 const Navbar = () => {
-    const store = useAppContext()
+  const store = useAppContext();
 
-    return (
-        <nav style={{ display: 'inline-block' }}>
-            <Ul>
-                <Li>
-                    <NavLink
-                        style={({ isActive }) => isActive ? activeLink : styleLink}
-                        to="/"
-                    >
-                        Inicio
-                    </NavLink>
-                </Li>
-                <Li>
-                    <NavLink
-                        style={({ isActive }) => isActive ? activeLink : styleLink}
-                        to="/articles"
-                    >
-                        Artículos
-                    </NavLink>
-                </Li>
-                {store.stateAuth &&
-                    <Li>
-                        <NavLink
-                            style={({ isActive }) => isActive ? activeLink : styleLink}
-                            to="/create"
-                        >
-                            Crear
-                        </NavLink>
-                    </Li>
-                }
-                
-                <Li>
-                    <NavLink
-                        style={({ isActive }) => isActive ? activeLink : styleLink}
-                        to="/about"
-                    >
-                        Sobre mi
-                    </NavLink>
-                </Li>
-                <Li>
-                    <NavLink
-                        style={({ isActive }) => isActive ? activeLink : styleLink}
-                        to="/contact"
-                    >
-                        Contacto
-                    </NavLink>
-                </Li>
-            </Ul>
-        </nav>
-    )
+  return (
+    <nav style={{ display: "inline-block" }}>
+      <Ul>
+        <Li>
+          <StyledNavLink to="/" exact>
+            Inicio
+          </StyledNavLink>
+        </Li>
+        <Li>
+          <StyledNavLink to="/articles">Artículos</StyledNavLink>
+        </Li>
+        {store.stateAuth && (
+          <Li>
+            <StyledNavLink to="/create">Crear</StyledNavLink>
+          </Li>
+        )}
+        <Li>
+          <StyledNavLink to="/about">Sobre mi</StyledNavLink>
+        </Li>
+        <Li>
+          <StyledNavLink to="/contact">Contacto</StyledNavLink>
+        </Li>
+      </Ul>
+    </nav>
+  )
 }
 
 export default Navbar
